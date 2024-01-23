8a7dd415bcb8fb1c45ce45412eb49afcc1a23049
LeonDur <115002318+LeonDur@users.noreply.github.com>
2024-01-23 10:54:19 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+			  const canvas = document.getElementById("canvas");
+			  const ctx = canvas.getContext("2d");
+			  const img = document.getElementById("slika");
+			  ctx.drawImage(img, 0, 0);
+			  };	
+			  
